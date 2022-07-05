ea08522a3e67f4e65b0b79e879f16bf7174f8de2
nostrss <jintagi@gmail.com>
2022-07-05 16:15:22 +0900

    add link

--- react-deploy/src/App.js
+++ react-deploy/src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>깃허브 페이지에 react 정적페이지 배포하기</p>
+        <h1>깃허브 페이지에 react 정적페이지 배포하기</h1>
+        <a
+          className='App-link'
+          href='https://github.com/nostrss/react-deploy/tree/main/react-deploy'
+        >
+          배포 방법 보기
+        </a>
       </header>
     </div>
   );
